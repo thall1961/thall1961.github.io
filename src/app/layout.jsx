db28605fe3f4720fb3fbf09a027860acc3ee76f3
Tom Hall <thall1961@gmail.com>
2023-11-06 15:52:50 -0600

    new site

---
+++ src/app/layout.jsx
@@ -0,0 +1,33 @@
+import { Providers } from '@/app/providers'
+import { Layout } from '@/components/Layout'
+
+import '@/styles/tailwind.css'
+
+export const metadata = {
+  title: {
+    template: '%s - Tom Hall',
+    default:
+      'Tom Hall - Software developer, founder, and amateur surfer',
+  },
+  description:
+    'I’m Tom, a software developer and entrepreneur based in Dallas. I founded and sold Electask, where I created a specialized task manager for elections and election workers.',
+  alternates: {
+    types: {
+      'application/rss+xml': `${process.env.NEXT_PUBLIC_SITE_URL}/feed.xml`,
+    },
+  },
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en" className="h-full antialiased" suppressHydrationWarning>
+      <body className="flex h-full bg-zinc-50 dark:bg-black">
+        <Providers>
+          <div className="flex w-full">
+            <Layout>{children}</Layout>
+          </div>
+        </Providers>
+      </body>
+    </html>
+  )
+}
